27c9fe712f64ae7fd2f01b918557151215a6f95a
leoben25 <147931123+leoben25@users.noreply.github.com>
2025-07-14 00:47:32 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
-
-  toggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
